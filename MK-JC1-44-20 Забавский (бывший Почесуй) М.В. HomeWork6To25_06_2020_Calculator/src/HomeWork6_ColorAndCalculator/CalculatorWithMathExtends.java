ef1cb143b9b0cfc1ccfab3aff8648bd3244e6e55
Maxim Pochesuj <pochesuj.mv@gmail.com>
2020-06-25 10:24:08 +0300

    firstcommit

---
+++ MK-JC1-44-20 Забавский (бывший Почесуй) М.В. HomeWork6To25_06_2020_Calculator/src/HomeWork6_ColorAndCalculator/CalculatorWithMathExtends.java
@@ -0,0 +1,38 @@
+package HomeWork6_ColorAndCalculator;
+
+public class CalculatorWithMathExtends extends CalculatorWithOperator {
+    public final double plus(double a, double b) {
+        double result = a + b;
+        return result;
+    }
+
+    public final double minus(double a, double b) {
+        double result = a - b;
+        return result;
+    }
+
+    public final double div(double a, double b) {
+        double result = a / b;
+        return result;
+    }
+
+    public final double mult(double a, double b) {
+        double result = a * b;
+        return result;
+    }
+
+    public double power(double a, double b) {
+        double result = Math.pow(a, b);
+        return result;
+    }
+
+    public double sqrt(double a) {
+        double result = Math.sqrt(a);
+        return result;
+    }
+
+    public double modul(double a) {
+        double result = Math.abs(a);
+        return result;
+    }
+}
---
+++ MK-JC1-44-20 Забавский (бывший Почесуй) М.В. HomeWork6To25_06_2020_Calculator/src/HomeWork6_ColorAndCalculator/MainCalculatorExtends.java
@@ -0,0 +1,18 @@
+package HomeWork6_ColorAndCalculator;
+
+public class MainCalculatorExtends {
+    public static void main(String[] args) {
+        CalculatorWithMathExtends calcExtends = new CalculatorWithMathExtends();
+        double div = calcExtends.div(28, 5);
+        double pow = calcExtends.power(div, 2);
+        double mult = calcExtends.mult(15, 7);
+        double n = 4.1;
+        float sum = (float) (n + mult + pow);
+        System.out.println(sum);
+        float try1 = sum / 0;
+        // исключение будет происходить только если типы переменных - int.
+        // Деление на 0.0 дает бесконечность о чём и написано в результате (Infinity)
+        float try2 = (float) (sum / 0.0d); // исключение будет происходить только если типы переменных - int.
+        // Деление на 0.0d дает бесконечность о чём и написано в результате (Infinity)
+    }
+}
